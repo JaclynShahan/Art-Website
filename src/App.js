e26aa6bea07f452fbda8827d492c00a9601bf808
Jaclyn <jaclyndshahan@gmail.com>
2019-10-24 20:06:32 -0500

    worked on gallery styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login.js';
-import {Layout} from 'antd';
+import {Layout, Icon} from 'antd';
 import router from './router';
 import {Link} from 'react-router-dom';
 
@@ -12,14 +12,17 @@ function App (props) {
     return(
       <div>
        <Layout>
+         <Header className="applogo">
+          <h1 className="logotitle">***MY COOL LOGO***</h1>
+         </Header>
          <Header className="navbar">
-                <Link to='/' className="links">Home</Link>
-                <Link to='/login' className="links">Login</Link> 
-                <Link to='/cart' className="links">Cart</Link>
+                <Link to='/login' className="links login">Login <Icon type="user"/> </Link> 
+                <Link to='/' className="links homelink">Gallery <Icon type="shop"/></Link>
+                <Link to='/cart' className="links cart">Cart <Icon type="shopping-cart"/></Link>
          </Header>
          
          <Content className="contents">Content</Content>
-         <Footer>Like me on www.facebook.com/blah</Footer>
+         <Footer className="footer"><h2> Like me on: www.facebook.com/blah   <Icon type="facebook"/></h2> <h2>Follow me at: www.twitter.com/blah  <Icon type="twitter"/></h2></Footer>
        </Layout>
         {router}
       </div>
