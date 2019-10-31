6d67fd2795d5306ed64bb54c050c9fef79cd9fed
Jaclyn <jaclyndshahan@gmail.com>
2019-10-30 20:17:31 -0500

    added upload feature

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,76 @@
 import React, { Component } from 'react'
 // import {Link} from 'react-router-dom';
 // import router from './router';
-import { Form, Icon, Input, Button, Checkbox, Avatar } from 'antd'
+import { Form, Icon, Input, Button, Checkbox, Avatar, Upload, message } from 'antd'
 import './Login.css'
 
 class Login extends Component {
-  super () {
-    this.state = {}
+  constructor() {
+    super()
+    this.state = {
+      loading: false
+    }
   }
 
+  getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  }
+  
+  beforeUpload(file) {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+  }
+
+  handleChange = info => {
+    if (info.file.status === 'uploading') {
+      this.setState({ loading: true });
+      return;
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      this.getBase64(info.file.originFileObj, imageUrl =>
+        this.setState({
+          imageUrl,
+          loading: false,
+        }),
+      );
+    }
+  };
   render () {
+    const uploadButton = (
+      <div>
+        <Icon type={this.state.loading ? 'loading' : 'plus'} />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
+    const { imageUrl } = this.state;
+  
     return (
       <div>
-        <form className='form-content'>
-          <div className='imgcontainer'>
+          {/* <div className='imgcontainer'>
             <Avatar size={74} icon='user' />
-          </div>
+          </div> */}
+          <form className='form-content'>
+          <Upload
+        name="avatar"
+        listType="picture-card"
+        className="avatar-uploader"
+        showUploadList={false}
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        beforeUpload={this.beforeUpload}
+        onChange={this.handleChange}
+      >
+        {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+      </Upload>
           <div className='container'>
             <label>Username:</label>
             <Input type='text' placeholder='Enter Username...' />
