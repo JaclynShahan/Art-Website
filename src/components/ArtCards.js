8a6f946b438005e8f6371606712f6fd797357df1
Jaclyn <jaclyndshahan@gmail.com>
2019-11-01 13:46:43 -0500

    changed upload feature to admin view

--- src/components/ArtCards.js
+++ src/components/ArtCards.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react'
 import Axios from 'axios'
 import { connect } from 'react-redux'
-import { Layout, Card, Icon, Button } from 'antd'
+import { Layout, Card, Icon, Button, Upload, message } from 'antd'
 
 class ArtCards extends Component {
   constructor () {
     super()
-    this.state = {}
+    this.state = {
+      loading: false
+    }
   }
 
- 
+  getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  }
+  
+  beforeUpload(file) {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+  }
+
+  handleChange = info => {
+    if (info.file.status === 'uploading') {
+      this.setState({ loading: true });
+      return;
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      this.getBase64(info.file.originFileObj, imageUrl =>
+        this.setState({
+          imageUrl,
+          loading: false,
+        }),
+      );
+    }
+  };
 
   render () {
+    const uploadButton = (
+      <div>
+        <Icon type={this.state.loading ? 'loading' : 'plus'} />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
+    const { imageUrl } = this.state;
     const { Header, Footer, Sider, Content } = Layout
     const { Meta } = Card
     console.log("This component's props:", this.props)
@@ -22,9 +63,20 @@ class ArtCards extends Component {
               key={this.props.id}
               className='cardspace'
               hoverable
-              style={{ width: 250, height: 380 }}
+              style={{ width: 300, height: 380 }}
               title={
-                <img src='https://www.keithmillsartist.com/uploads/1/0/7/1/10719910/between-the-rivers_orig.jpg' />
+                <Upload
+        name="avatar"
+        listType="picture-card"
+        className="avatar-uploader"
+        showUploadList={false}
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        beforeUpload={this.beforeUpload}
+        onChange={this.handleChange}
+      >
+        {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+      </Upload>
+                // <img src='https://www.keithmillsartist.com/uploads/1/0/7/1/10719910/between-the-rivers_orig.jpg' />
               }
             >
             
